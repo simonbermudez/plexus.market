3dc817dac5f8e2af27d715478e15596d1d750e8a
Simon Bermudez <sabdz55@gmail.com>
2020-06-20 13:01:04 -0400

    list playlists as cards

--- src/components/Playlists.vue
+++ src/components/Playlists.vue
@@ -1,11 +1,22 @@
 <template>
-  <div class="playlists">
-    <div v-for="playlist in playlists" :key="playlist.id">
-      {{ playlist.snippet.title }}
-      <img
-        :src="playlist.snippet.thumbnails.standard.url"
-        @click="$emit('select-playlist', playlist.id)"
-      />
+  <div id="playlists" class="row">
+    <div
+      v-for="playlist in playlists"
+      :key="playlist.id"
+      class="col-sm-6"
+      @click="$emit('select-playlist', playlist.id)"
+    >
+      <div class="card bg-dark text-white mt-5">
+        <img
+          class="card-img"
+          :src="playlist.snippet.thumbnails.standard.url"
+          :alt="playlist.snippet.title"
+        />
+        <div class="card-img-overlay">
+          <h5 class="card-title">{{ playlist.snippet.title }}</h5>
+          <p class="card-text">{{ playlist.snippet.description }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
