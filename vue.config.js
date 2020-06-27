bbc77ad184fc62f7e502b2f110d52efaaa8c8ccf
Daniel Esteves <estevesd8@gmail.com>
2020-06-26 23:12:00 -0400

    add: pwa features

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // ...other vue-cli plugin options...
+  pwa: {
+    name: 'Plexus Market',
+    themeColor: '#1c64f2',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths: {
+      favicon64: 'img/icons/favicon-64x64.png',
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-icon-144x144.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/ms-icon-144x144.png',
+    },
+  },
+}
